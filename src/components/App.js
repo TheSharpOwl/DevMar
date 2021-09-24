9ec92e7beb84a59b848f86b1556434bb64fb6582
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-09-24 11:49:49 +0300

    Fixed duplicated navbar.

--- src/components/App.js
+++ src/components/App.js
@@ -53,16 +53,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://thesharpowl.github.io/tabs/about/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            DevMar
-          </a>
-        </nav>
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://thesharpowl.github.io/tabs/about/"
           target="_blank"
           rel="noopener noreferrer"
         >
